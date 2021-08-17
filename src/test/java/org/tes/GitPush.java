9565d7ea6c1da108452d0370541df1cd7c4b7109
manojkanna1 <manojkanna093@gmail.com>
2021-08-17 23:32:46 +0530

    sample push

---
+++ src/test/java/org/tes/GitPush.java
@@ -0,0 +1,14 @@
+package org.tes;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GitPush {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://en-gb.facebook.com/");
+}
+}
